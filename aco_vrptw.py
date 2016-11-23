729cac1fa64d0271fff99323ee3ed202cd0c9e58
Lada Zednik <lzednik@gmail.com>
2016-11-23 12:04:25 -0600

    update

--- aco_vrptw.py
+++ aco_vrptw.py
@@ -12,8 +12,8 @@ locCount=len(dataM)
 initSolution=initSolution(depo,dataM,distM)
 
 
+phiM0= [[float(1)/initSolution['distance'] for i in range(locCount)] for j in range(locCount)]
 phiM1= [[float(1)/initSolution['distance'] for i in range(locCount)] for j in range(locCount)]
-
 
 
 vehicleCount=initSolution['vehicleCount']
@@ -21,20 +21,58 @@ vehicleCount=initSolution['vehicleCount']
 tour=initSolution['tour']
 tour_fl=initSolution['tour_fl']
 
-for iteration in range(300):
+visitedArcs=[]
+bestArcs=[]
+bestSolution=initSolution
 
-    ants=Ant(vehicleCount=vehicleCount,dataM=dataM)
-    solution=ants.calculate(dataM,distM,phiM1,depo,tour,tour_fl)
-    
-    #Full Solution
-    if solution['visitedCount']==locCount-1:
-        vehicleCount-=1
-        tour=solution['tour']
-        tour_fl=solution['tour_fl']
+
+for iteration in range(100):
+    visitedArcs=[]
+    for colony in range(10):
+
+        ants=Ant(vehicleCount=vehicleCount,dataM=dataM)
+        solution=ants.calculate(dataM,distM,phiM1,depo,tour,tour_fl)
+        
+        #Full Solution
+        if solution['visitedCount']==locCount-1:
+            vehicleCount-=1
+            tour=solution['tour']
+            tour_fl=solution['tour_fl']
+            
+            for loc in tour:
+                visitedArcs.append((loc,tour[loc]))
+            for loc in tour_fl:
+                visitedArcs.append((depo,loc))
+
+            if bestSolution['vehicleCount']>solution['vehicleCount']:
+                bestSolution=solution
+                for loc in tour:
+                    bestArcs.append((loc,tour[loc]))
+                for loc in tour_fl:
+                    bestArcs.append((depo,loc))
+
+            print('iteration\t',iteration)
+            print('colony\t\t',colony)
+            print('vehicleCount\t',solution['vehicleCount'])
+            print('****************\n')
+
+
+    visitedArcs=list(set(visitedArcs))
+    #evaporate
+    alpha=0.1
+
+    for arc in visitedArcs:
+        locFrom=arc[0]
+        locTo=arc[1]
+        phiM1[locFrom][locTo]=(1-alpha)*phiM1[locFrom][locTo]+alpha*phiM0[locFrom][locTo]
+
+    for arc in bestArcs:
+        locFrom=arc[0]
+        locTo=arc[1]
+        phiM1[locFrom][locTo]=(1-alpha)*phiM1[locFrom][locTo]+alpha/bestSolution['distance']
+
+    if iteration%50==0:
+        print('*******************')
         print('iteration\t',iteration)
-        print('vehicleCount\t',solution['vehicleCount'])
-        print('****************\n')
-#for vehicle in solution['vehicles']:
-#    print(vehicle['tour'])
-
+        print('*******************')
 print('run finished')
