4488e15cc1b8662496e06d7fd4622a7e5ca0ec1e
Lada Zednik <lzednik@gmail.com>
2016-12-08 20:36:06 -0600

    update

--- sql_create_dtb.py
+++ sql_create_dtb.py
@@ -5,30 +5,39 @@ conn=sqlite3.connect(dtb)
 c=conn.cursor()
 
 #c.execute('DROP TABLE Phi')
-c.execute('DROP TABLE Vehicles')
+#c.execute('DROP TABLE Vehicles')
 #c.execute('DROP TABLE Attr')
 #c.execute('DROP TABLE Summary')
 #c.execute('DROP TABLE FIN')
-c.execute('DROP TABLE Solutions')
+#c.execute('DROP TABLE Solutions')
+
+
+c.execute('''
+    CREATE TABLE Run_Summary(   run INTEGER,
+                                vehCount INTEGER,
+                                distance INTEGER)
+''')
+
+
 
 #Solutions
-c.execute('''
-    CREATE TABLE Solutions( iter INTEGER,
-                            colony INTEGER,
-                            vehCount INTEGER,
-                            visitedCount INTEGER,
-                            visited TEXT
-                            )
-''')
+#c.execute('''
+#    CREATE TABLE Solutions( iter INTEGER,
+#                            colony INTEGER,
+#                            vehCount INTEGER,
+#                            visitedCount INTEGER,
+#                            visited TEXT
+#                            )
+#''')
 
 
 ##Vehicles
-c.execute('''
-    CREATE TABLE Vehicles(  iter INTEGER,
-                            colony INTEGER,
-                            vehNum INTEGER,
-                            tour TEXT)
-''')
+#c.execute('''
+#    CREATE TABLE Vehicles(  iter INTEGER,
+#                            colony INTEGER,
+#                            vehNum INTEGER,
+#                            tour TEXT)
+#''')
 
 
 
